0747b846af6edeae8506539beee29d634b8f4933
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-12-28 21:10:47 +0700

    Create truu-tuong.php

---
+++ b2ytb/truu-tuong.php
@@ -0,0 +1,52 @@
+<?php
+//chuyen hoa doi tuong/su vat/su viec o de bai cua bai toan hoac cac cong cu can su dung
+//qua trinh lam ra san pham(code) thanh cac cau lenh class
+//xac dinh thuoc tinh/hanh dong can thiet cua doi tuong thuc te <=>thuoc tinh/phuong thuc
+class SinhVien
+{
+    var $hoten;
+    var $ngaysinh;
+    var $gioitinh;
+    var $tuoi;
+    var $sdtbo;
+    var $mssv;
+    var $cmnd;
+    var $avatar;
+    var $bangcap3;
+    // var $sdtanh;
+    // var $sdtchi;
+    var $diachi;
+    var $sdt;
+    // var $tinhtranghonnhan;
+    // var $chieucao;
+    // var $cannang;
+    // var $benhditruyen;
+    // var $mautoc;
+    // var $benhtruyennhiem;
+
+    // function an(){}
+    // function ngu(){}
+    function dihoc()
+    {
+    }
+    function hoclai()
+    {
+    }
+    function dithi()
+    {
+    }
+    function lamquiz()
+    {
+    }
+    function lamlab()
+    {
+    }
+    // function henho(){}
+    // function totinh(){}
+    // function dabong(){}
+    // function game(){}
+    // function trada(){}
+    function diemdanh()
+    {
+    }
+}
