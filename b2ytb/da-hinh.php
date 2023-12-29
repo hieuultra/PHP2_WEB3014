2f252cf1760f9385fb22231b8c4ffd9fb861672c
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-12-29 21:03:22 +0700

    Create da-hinh.php

---
+++ b2ytb/da-hinh.php
@@ -0,0 +1,31 @@
+<?php
+// tính chất đa hình (áp dụng cho các lớp có quan hệ kế thừa)
+// tính chất này thể hiện qua từ khóa overwrite (ghi đè/định nghĩa lại) cac thuộc tính & phương thức
+// đã được định nghia ở lớp cha
+class SinhVien
+{
+    function say()
+    {
+        echo "hello world!";
+    }
+}
+class Animal extends SinhVien
+{
+}
+
+class Duck extends Animal
+{
+    function say()
+    {
+        echo "cạp cạp<br>";
+    }
+}
+
+class Chicken extends Animal
+{
+}
+
+$vinh = new Duck();
+$vinh->say();
+$nam = new Chicken();
+$nam->say();
