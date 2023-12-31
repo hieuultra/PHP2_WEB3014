41113b5d8346e85bc5459681daf432a6927d96c3
hieuultra <147162531+hieuultra@users.noreply.github.com>
2023-12-31 11:23:12 +0700

    mvc

---
+++ mvc/app/controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+class HomeController
+{
+    public function index()
+    {
+        return 'trang chu website';
+    }
+    public function detail()
+    {
+        return "trang chi tiet 1 sp";
+    }
+    public function contact()
+    {
+        return "trang lien he du an";
+    }
+}
--- mvc/index.php
+++ mvc/index.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
 
 // tôi muốn: "tất cả các request tới thư mục mvc phải bắt buộc đi qua file index.php"
 // thu thập các url gửi lên project
+$url = isset($_GET['url']) ? $_GET['url'] : "/";
+echo $url;
