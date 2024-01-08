f7c5d89e15de2b7eaff9cecc518dbfcc307d25ad
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-01-08 12:46:26 +0700

    update

--- lab1/bai1.php
+++ lab1/bai1.php
@@ -21,8 +21,8 @@ $list_of_course= get_courses();
 $semester=(!empty($_GET['semester']) ? $_GET['semester']:'');
 $course_name= find_by_semester($semester);
 $page_content=$course_name;
-
-//view
+?>
+<!-- view -->
 <?= $page_content ?>
 <select name="course" id="">
 <?php foreach($list_of_course as $course_name): ?>
