f2c2a4da95bd75d6bb08d6e8c3b4928b0841a805
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-01-25 10:33:30 +0700

    up

--- lab1/bai2/view.php
+++
@@ -1,6 +0,0 @@
-<?= $page_content ?>
-<select name="course" id="">
-    <?php foreach ($list_of_course as $course_name) : ?>
-        <option value=""><?= $course_name ?></option>
-    <?php endforeach; ?>
-</select>
--- lab1/bai3/view.php
+++
@@ -1,5 +0,0 @@
-<?= $user['first_name'] . "" . $user['last_name'] ?>
-<form action="" method="post">
-    <input type="email" name="" id="">
-    <input type="submit" value="">
-</form>
