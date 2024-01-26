86f5f0930e1dd4a0a3f8c86a2bc212a9b1626113
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-01-27 00:14:11 +0700

    up

---
+++ asm2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hieu/mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b53f7022601cfea4a486caaf24bcdb9a0bd55708',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hieu/mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b53f7022601cfea4a486caaf24bcdb9a0bd55708',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phroute/phroute' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c1893b885966a0c7b50b9239dd867fda7a312dfa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phroute/phroute',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
