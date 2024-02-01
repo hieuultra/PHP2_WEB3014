b73e4e9deb59eb0322e448c78f2545cdc3597da6
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-02-01 21:38:56 +0700

    up

--- asm2_1/user/app/views/forgotPassword/forgot-password.php
+++ asm2_1/user/app/views/forgotPassword/forgot-password.php
@@ -9,6 +9,24 @@
   <meta name="author" content="Bootlab" />
 
   <title>Reset Password - AppStack - Admin &amp; Dashboard Template</title>
+  <!-- fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <!-- Favicon -->
+  <link href="./app/img/favicon.ico" rel="icon" />
+
+  <!-- Google Web Fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+  <!-- Font Awesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+
+  <!-- Libraries Stylesheet -->
+  <link href="./app/views/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+
+  <!-- Customized Bootstrap Stylesheet -->
+  <link href="./app/views/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="./app/css/popup.css">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" />
 </head>
@@ -50,6 +68,95 @@
       <h5 class="m-0">+012 345 6789</h5>
     </div>
   </div>
+  <!-- Navbar Start -->
+  <div class="container-fluid bg-dark mb-30">
+    <div class="row px-xl-5">
+      <div class="col-lg-3 d-none d-lg-block">
+        <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
+          <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Categories</h6>
+          <i class="fa fa-angle-down text-dark"></i>
+        </a>
+        <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
+          <div class="navbar-nav w-100">
+            <!-- <?php
+                  foreach ($listCat as $c) {
+                    extract($c);
+                    // $linkdm = "index.php?act=product_cat&id_cat=" . $id_cat;
+                    echo '
+  <a href="' . $linkdm . '" class="dropdown-item">' . $name . '</a>
+  ';
+                  }
+                  ?> -->
+          </div>
+        </nav>
+      </div>
+      <div class="col-lg-9">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+          <a href="" class="text-decoration-none d-block d-lg-none">
+            <span class="h1 text-uppercase text-dark bg-light px-2">Ultra</span>
+            <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">Shop</span>
+          </a>
+          <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav mr-auto py-0">
+              <a href="./" class="nav-item nav-link active">Home</a>
+              <a href="?act=shop" class="nav-item nav-link">Shop</a>
+
+              <div class="nav-item dropdown">
+                <a href="" class="nav-link dropdown-toggle" data-toggle="dropdown">Brands</a>
+                <div class="dropdown-menu rounded-0 m-0">
+                  <!-- <?php
+                        foreach ($dsbr as $br) {
+                          extract($br);
+                          $linkdm = "index.php?act=product_brand&id_brand=" . $id_brand;
+                          echo '
+                <a href="' . $linkdm . '" class="dropdown-item">' . $name_brand . '</a>
+                ';
+                        }
+                        ?> -->
+                </div>
+              </div>
+              <a href="?act=contact" class="nav-item nav-link">Contact</a>
+              <a href="?act=blog" class="nav-item nav-link">Blog</a>
+              <a href="?act=sell" class="nav-item nav-link">Selling Products</a>
+            </div>
+            <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
+              <a href="?act=view_wishlist" class="btn px-0">
+                <i class="fas fa-heart text-primary"></i>
+                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+              </a>
+              <a href="?act=view_cart" class="btn px-0 ml-3">
+                <i class="fas fa-shopping-cart text-primary"></i>
+                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+              </a>
+              <?php
+              if (isset($_SESSION['auth'])) {
+              ?>
+                <a href="account" class="btn px-0">
+                  <i class="fa-regular fa-user text-primary"></i>
+                  <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;"></span>
+                </a>
+              <?php
+              } else {
+              ?>
+                <a href="login" class="btn px-0">
+                  <i class="fa-regular fa-user text-primary"></i>
+                  <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;"></span>
+                </a>
+              <?php } ?>
+              <a href="?act=mybill" class="btn px-0">
+                <i class="fas fa-file-invoice-dollar text-primary"></i>
+                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <!-- Navbar End -->
   <main class="main d-flex w-100 mt-5">
     <div class="container d-flex flex-column">
       <div class="row h-100">
